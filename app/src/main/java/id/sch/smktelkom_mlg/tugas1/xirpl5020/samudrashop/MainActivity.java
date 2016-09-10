152c5d64fab27aabb452ede88140347b85694d20
Samudra~ <miqbalsamudra@gmail.com>
2016-09-10 13:01:33 +0700

    Pertama

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl5020/samudrashop/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.tugas1.xirpl5020.samudrashop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
