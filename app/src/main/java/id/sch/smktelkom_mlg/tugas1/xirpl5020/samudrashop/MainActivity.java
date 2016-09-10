8ffc3896cded0ba6e381b2cff1fc8f9c8c2e142d
Samudra~ <miqbalsamudra@gmail.com>
2016-09-10 14:47:45 +0700

    Kedua

--- app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl5020/samudrashop/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas1/xirpl5020/samudrashop/MainActivity.java
@@ -1,13 +1,79 @@
 package id.sch.smktelkom_mlg.tugas1.xirpl5020.samudrashop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama, etTelepon;
+    Button btPesan;
+    TextView tvHasil, tvData;
+    Spinner spProvinsi, spKota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTelepon = (EditText) findViewById(R.id.editTextTelepon);
+        btPesan = (Button) findViewById(R.id.buttonPesan);
+        tvData = (TextView) findViewById(R.id.textViewData);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        spProvinsi = (Spinner) findViewById(R.id.spinnerProvinsi);
+        spKota = (Spinner) findViewById(R.id.spinnerKota);
+        String[][] arKota = {
+                {"Kabupaten Bangkalan", "Kabupaten Banyuwangi", "Kabupaten Blitar", "Kabupaten Bojonegoro", "Kabupaten Bondowoso", "Kabupaten Gresik", "Kabupaten Jember", "Kabupaten Jombang", "Kabupaten Kediri", "Kabupaten Lamongan", "Kabupaten Lumajang", "Kabupaten Madiun", "Kabupaten Magetan", "Kabupaten Malang", "Kabupaten Mojokerto", "Kabupaten Nganjuk", "Kabupaten Ngawi", "Kabupaten Pacitan", "Kabupaten Pamekasan", "Kabupaten Pasuruan", "Kabupaten Ponorogo", "Kabupaten Probolinggo", "Kabupaten Sampang", "Kabupaten Sidoarjo", "Kabupaten Situbondo", "Kabupaten Sumenep", "Kabupaten Trenggalek", "Kabupaten Tuban", "Kabupaten Tulungagung", "Kota Batu", "Kota Blitar", "Kota Kediri", "Kota Madiun", "Kota Malang", "Kota Mojokerto", "Kota Pasuruan", "Kota Probolinggo", "Kota Surabaya"},
+                {"Kabupaten Banjarnegara", "Kabupaten Banyumas", "Kabupaten Batang", "Kabupaten Blora", "Kabupaten Boyolali", "Kabupaten Brebes", "Kabupaten Cilacap", "Kabupaten Demak", "Kabupaten Grobogan", "Kabupaten Jepara", "Kabupaten Karanganyar", "Kabupaten Kebumen", "Kabupaten Kendal", "Kabupaten Klaten", "Kabupaten Kudus", "Kabupaten Magelang", "Kabupaten Pati", "Kabupaten Pekalongan", "Kabupaten Pemalang", "Kabupaten Purbalingga", "Kabupaten Purwokerto", "Kabupaten Rembang", "Kabupaten Semarang", "Kabupaten Sragen", "Kabupaten Sukoharjo", "Kabupaten Tegal", "Kabupaten Temanggung", "Kabupaten Wonogiri", "Kabupaten Wonosobo", "Kota Magelang", "Kota Pekalongan", "Kota Salatiga", "Kota Semarang", "Kota Surakarta", "Kota Tegal"},
+                {"Kabupaten Bandung", "Kabupaten Bandung Barat", "Kabupaten Bekasi", "Kabupaten Bogor", "Kabupaten Ciamis", "Kabupaten Cianjur", "Kabupaten Cirebon", "Kabupaten Garut", "Kabupaten Indramayu", "Kabupaten Karawang", "Kabupaten Kuningan", "Kabupaten Majalengka", "Kabupaten Pangandaran", "Kabupaten Purwakarta", "Kabupaten Subang", "Kabupaten Sukabumi", "Kabupaten Sumedang", "Kabupaten Tasikmalaya", "Kota Bandung", "Kota Banjar", "Kota Bekasi", "Kota Bogor", "Kota Cimahi", "Kota Cirebon", "Kota Depok", "Kota Sukabumi", "Kota Sukabumi", "Kota Tasikmalaya"},
+        };
+
+        btPesan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doHasil();
+            }
+        });
     }
+
+    private void doHasil() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String telepon = etTelepon.getText().toString();
+            String provinsi = spProvinsi.getSelectedItem().toString();
+            tvData.setText("\nData Pembeli");
+
+            nama = etNama.getText().toString();
+            telepon = etTelepon.getText().toString();
+            provinsi = spProvinsi.getSelectedItem().toString();
+            tvHasil.setText("Nama : " + nama + "\nTelepon : " + telepon + "\nProvinsi " + provinsi);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        String telepon = etTelepon.getText().toString();
+        if (telepon.isEmpty()) {
+            etTelepon.setError("Nomor Telepon belum diisi");
+            valid = false;
+        } else {
+            etTelepon.setError(null);
+        }
+        return valid;
+    }
+
 }
